eacfb2f1cbe5e12e0651c7202ee12a79cef34b77
Bill Shannon <bill@billshannon.com>
2015-08-07 13:42:17 -0600

    tests pass

--- house.js
+++ house.js
@@ -1,5 +1,19 @@
-function House (numberOfDoors) {
+function House(numberOfDoors, numberOfWindows) {
     this.numberOfDoors = numberOfDoors;
+    this.numberOfWindows = numberOfWindows;
+    this.toString = function () {
+        if (numberOfDoors > 1 && numberOfWindows === 1) {
+            return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " window";
+        } else if (numberOfDoors > 1 && numberOfWindows > 1) {
+            return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " windows";
+        } else if (numberOfDoors === 1 && numberOfWindows > 1) {
+            return "The house has " + this.numberOfDoors + " door and " + this.numberOfWindows + " windows";
+        } else if (numberOfDoors === 1 && numberOfWindows === 1) {
+            return "The house has " + this.numberOfDoors + " door and " + this.numberOfWindows + " window";
+        } else if (numberOfDoors < 1 && numberOfWindows < 1) {
+            return "The house has " + this.numberOfDoors + " doors and " + this.numberOfWindows + " windows";
+        }
+    }
 
 }
 
